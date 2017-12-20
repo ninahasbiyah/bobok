f11afcbe3efcd3855db684996bdd10dbae552738
ninahasbiyah <ninahasbiyah1998@gmail.com>
2017-12-20 15:53:19 +0700

    skak1

---
+++ skak.py
@@ -0,0 +1,14 @@
+print('-----------------------')	
+print('Gaji dalam waktu 21 hari')
+n = int(input('Masukan Jumlah Karyawan yang ingin di hitung gajinya: '))
+print()
+for i in range (1,n+1) :
+	nama = str(input('Masukan Nama :'))
+	jam = int(input('Masukan Jumlah Jam :'))
+	
+	#jika jam kerja lebih dari 504 jam maka akan masuk kedalam jam lembur
+	if jam >= 504 :
+		lembur = jam - 504
+		upah = 504 * 20000 + lembur * 25000
+	else :
+		upah = jam*20000
